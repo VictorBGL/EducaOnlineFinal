e65c6564e9447803388f3060d0be2ab46da6dea3
Victor Lino <victor.goncallino@gmail.com>
2026-01-24 14:11:13 -0300

    teste exclusao api pedido

--- frontend/libs/services/src/lib/configuration/environment.ts
+++ frontend/libs/services/src/lib/configuration/environment.ts
@@ -1,7 +1,19 @@
 export const environment = {
   production: false,
-  apiIdentidade: "https://localhost:44371/api",
-  apiConteudo: "https://localhost:44394/api",
-  alunoBff: "https://localhost:44356/api",
-  apiAluno: "https://localhost:44307/api"
+
+  apiIdentidade: "http://localhost:5001/api",
+  apiConteudo: "http://localhost:5002/api",
+  apiAluno: "http://localhost:5003/api",
+  apiPedidos: "http://localhost:5004/api",
+  apiFinanceiro: "http://localhost:5005/api",
+  alunoBff: "http://localhost:5000/api",
+  rabbitMqManagement: "http://localhost:15672",
+
+  //DEV LOCAL
+  // apiIdentidade: "https://localhost:7070/api",
+  // apiConteudo: "https://localhost:7183/api",
+  // apiAluno: "https://localhost:7094/api",
+  // apiPedidos: "https://localhost:7244/api",
+  // apiFinanceiro: "https://localhost:7059/api",
+  // alunoBff: "https://localhost:7093/api",
 };
